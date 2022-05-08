8eff4f198f25c323e7e05228ca8c206d2c9a8e8b
supertgo <thiagoroberto2003@gmail.com>
2022-05-08 08:09:11 -0300

    feat(pages): add revalidade to static pages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,22 @@
-import { MapProps } from 'components/Map';
+import { GetStaticProps } from 'next';
 import client from 'graphql/client';
 import { GetPlacesQuery } from 'graphql/generated/graphql';
 import { GET_PLACES } from 'graphql/queries';
+
+import { MapProps } from 'components/Map';
 import HomeTemplate from 'templates/Home';
 
 export default function Home({ places }: MapProps) {
   return <HomeTemplate places={places} />;
 }
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const { places } = await client.request<GetPlacesQuery>(GET_PLACES);
 
   return {
     props: {
       places
-    }
+    },
+    revalidate: 60
   };
 };
--- src/pages/place/[slug].tsx
+++ src/pages/place/[slug].tsx
@@ -42,6 +42,7 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
   return {
     props: {
       place
-    }
+    },
+    revalidate: 60
   };
 };
