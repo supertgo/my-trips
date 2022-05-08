a40c94e0cce3543c9c23ff1ebd8990fb31eaf0be
supertgo <thiagoroberto2003@gmail.com>
2022-05-08 09:58:49 -0300

    feat(components): add custom zoom to mobile viewport

--- src/components/Map/index.tsx
+++ src/components/Map/index.tsx
@@ -1,5 +1,5 @@
 import { useRouter } from 'next/router';
-import { MapContainer, TileLayer, Marker } from 'react-leaflet';
+import { MapContainer, TileLayer, Marker, MapConsumer } from 'react-leaflet';
 
 import * as S from './styles';
 
@@ -45,7 +45,25 @@ const Map = ({ places }: MapProps) => {
         zoom={3}
         minZoom={3}
         style={{ height: '100%', width: '100%' }}
+        maxBounds={[
+          [-180, 180],
+          [180, -180]
+        ]}
       >
+        <MapConsumer>
+          {(map) => {
+            const width =
+              window.innerWidth ||
+              document.documentElement.clientWidth ||
+              document.body.clientWidth;
+
+            if (width < 768) {
+              map.setMinZoom(2);
+            }
+
+            return null;
+          }}
+        </MapConsumer>
         <CustomTileLayer />
 
         {places?.map(({ id, name, location, slug }) => {
